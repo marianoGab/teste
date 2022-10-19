dbee23a3ebd92dda238d2c45a47a2de78b3d22f2
marianoGab <mariano.teste2q@gmail.com>
2022-10-19 11:14:38 -0300

    :sparkles: Adiciona verificacao de idade

---
+++ Codigos/teste.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int idade;
+  printf("Hello World\n");
+
+  printf("Qual e sua idade?\n");
+  scanf("%d", &idade);
+
+  printf("Voce tem %d anos!\n", idade);
+  return 0;
+}
