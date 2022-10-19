23609cba19937bb4f204661090fa195fb4e1f997
marianoGab <mariano.teste2q@gmail.com>
2022-10-19 11:20:53 -0300

    :sparkles: Adiciona ano de nascimento

--- Codigos/teste.c
+++ Codigos/teste.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#define ANO_ATUAL 2022
 
 int main(void)
 {
-  int idade;
+  int idade, ano;
   printf("Hello World\n");
 
   printf("Qual e sua idade?\n");
   scanf("%d", &idade);
 
   printf("Voce tem %d anos!!!!!\n", idade);
+
+  ano = ANO_ATUAL - idade;
+  printf("Voce nasceu em %d!!!!!\n", ano);
   return 0;
 }
